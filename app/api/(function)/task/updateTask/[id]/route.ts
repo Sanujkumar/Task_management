2eef5f5629064dab30f54456434daf1f132790cc
sanuj <sanujkumar67943@gmail.com>
2025-05-30 10:01:57 +0530

    api taks update fix error

--- app/api/(function)/task/updateTask/[id]/route.ts
+++ app/api/(function)/task/updateTask/[id]/route.ts
@@ -1,40 +1,44 @@
 import { PrismaClient } from "@/app/generated/prisma"; 
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
+
 const secret = process.env.AUTH_SECRET;
 const prisma = new PrismaClient();
 
- export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-    const token = await getToken({ req, secret });
-    console.log("tokenTask",token);  
-    
-  if(!token  || !token.id){
-    return NextResponse.json({
-      message: "user not authorized"  
-    }, {status:400});
-  }
-    const  params  = await context.params;
-    const taskId = Number(params.id);
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+  const token = await getToken({ req, secret });
+  console.log("tokenTask", token);
 
-    const body = await req.json();
-    const {title, description, date, priority, status } = body;
-  
-    try {
-      const updated = await prisma.task.updateMany({
-        where: {
-          id: taskId,  
-        },
-        data: {
-          title,
-          description,
-          date: new Date(date),   
-          priority,
-          status,
-        },  
-      });
-  
-      return NextResponse.json({ message: "Task updated successfully" },{status:200});
-    } catch (error) {
-      return NextResponse.json({ error: "Failed to update task" }, { status: 500 });
-    }
-  }  
+  if (!token || !token.id) {
+    return NextResponse.json(
+      { message: "user not authorized" },
+      { status: 400 }
+    );
+  }
+
+  const taskId = Number(params.id);
+  const body = await req.json();
+  const { title, description, date, priority, status } = body;
+
+  try {
+    const updated = await prisma.task.updateMany({
+      where: {
+        id: taskId,
+      },
+      data: {
+        title,
+        description,
+        date: new Date(date),
+        priority,
+        status,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Task updated successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update task" }, { status: 500 });
+  }
+}
