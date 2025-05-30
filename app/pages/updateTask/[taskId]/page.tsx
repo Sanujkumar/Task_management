8161afd24d6383904d35351e1ac92d75ce9ab1b6
sanuj <sanujkumar67943@gmail.com>
2025-05-30 10:23:24 +0530

    update frontend task

--- app/pages/updateTask/[taskId]/page.tsx
+++ app/pages/updateTask/[taskId]/page.tsx
@@ -6,14 +6,17 @@ import { useParams } from "next/navigation";
 export default function updateTask() {  
     const params = useParams();
     const taskId = params?.taskId;
+
     try {
         const EditTask = async ({taskData}:any) => {
             await axios.put(`http://localhost:3000/api/task/updateTask/${taskId}`, taskData, { withCredentials: true })
         }
+
+        return (
+        <CreateTask topTitle="EditTask" buttonName="Update" onSubmit={EditTask} />
+    )   
     }catch(err){
         console.log("Failed to update task",err);  
     }
-    return (
-        <CreateTask topTitle="EditTask" buttonName="Update" onSubmit={EditTask} />
-    )
+    
 }        
