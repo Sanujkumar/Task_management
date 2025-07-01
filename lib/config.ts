eb30df21e92d7415c3aaad73450f24499f6f7361
sanuj <sanujkumar67943@gmail.com>
2025-07-01 08:16:38 +0530

    update url

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 
 
-export const Url = "https://task-management-xztj.vercel.app"   
-// export const production_url = "https://task-management-xztj.vercel.app"  
+export const Url = "https://task-management-gamma-bay-41.vercel.app"     
+// export const production_url = "https://task-management-gamma-bay-41.vercel.app"  
 // export const Url = "http://localhost:3000";    
      
        
