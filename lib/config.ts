53bcb11ebc6c69c99617057ff37592c225da587b
sanuj <sanujkumar67943@gmail.com>
2025-07-05 19:46:47 +0530

    add url

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,8 @@
 
 
-export const Url = "http://localhost:3000"     
+export const Url = "https://task-management-gamma-bay-41.vercel.app"     
 // export const production_url = "https://task-management-gamma-bay-41.vercel.app"  
 // export const Url = "http://localhost:3000";    
-     
+        
        
                                   
